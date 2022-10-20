e4e4d3dbbd0348255fd3332a3f83370f1a286014
Twardykolo <adamus8895@gmail.com>
2022-10-20 02:36:35 +0200

    fix: migrationMailAttemptsIdMissmatchedType

--- src/main/resources/db/migration/V20220808181444__create_mail_attempt_table.sql
+++ src/main/resources/db/migration/V20220808181444__create_mail_attempt_table.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA logs;
 
 CREATE TABLE logs.mail_attempts
 (
-    id              BIGINT  NOT NULL,
+    id              BIGSERIAL  NOT NULL,
     attempt_date            TIMESTAMP WITH TIME ZONE NOT NULL,
     requesting_user         text NULL,
     body                    text NOT NULL,
